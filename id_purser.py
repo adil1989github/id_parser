99161008400a2609bf0fce47467dd63faa3c803c
adil1989github <87062463+adil1989github@users.noreply.github.com>
2022-10-17 08:35:13 +0300

    Update id_purser.py

--- id_purser.py
+++ id_purser.py
@@ -3,13 +3,13 @@ def sum_id(id_customer: str):
 
 
 def counter_customer_start_0(n_customer: int):
-    for i, j in dict_group.item():
+    for i, j in dict_group.items():
         if len(j) == n_customer:
             return i
 
 
 def counter_customer(n_customer: int, n_first_id: str):
-    for i, j in dict_group.item():
+    for i, j in dict_group.items():
         if len(j) == n_customer and j[0] == n_first_id:
             return i
 
@@ -28,15 +28,13 @@ def group_funk(list_id):
 if __name__ == "__main__":
     # Заполните свой список id
     example_id_list = ['7412567', '7415267', '1234567']
-    # Из списка формирует словари. Пример: {32: [7412567, 7415267], 28: [1234567]}
-    dict_group = group_funk(list_id)
+    # Из списка формирует словари. Пример: {32: ['7412567', '7415267'], 28: ['1234567']}
+    dict_group = group_funk(example_id_list)
     # num_customer количество клиентов
     num_customer: int = 1
     # num_first_id первый id в списке
     num_first_id: str = ''
-    if n_first_id == '':
+    if num_first_id == '':
         print(counter_customer_start_0(num_customer))
     else:
         print(counter_customer(num_customer, num_first_id))
-
-
